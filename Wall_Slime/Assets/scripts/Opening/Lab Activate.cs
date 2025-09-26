0bac36b0ed1cd8b8be2cd85435d828751276d6fb
Duderz06 <zachary.cooke@ontariotechu.net>
2025-09-26 00:55:58 -0400

    lab on / tutorial
    added a trigger to turn on the lab and made a small tutorial level

---
+++ Wall_Slime/Assets/scripts/Opening/Lab Activate.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+using System.Collections;
+
+public class LabActivate : MonoBehaviour
+{
+
+    private bool PlayerActivated=false;
+    public bool LabOn=false;
+
+    public GameObject DarknessOverlay;
+    public GameObject LabStuff;
+
+
+    void Update()
+    {
+
+
+        DarknessOverlay.SetActive(!LabOn);
+        LabStuff.SetActive(LabOn);
+
+
+
+
+
+
+    }
+
+
+
+
+    public void OnTriggerEnter(Collider col) {
+
+
+        if (col.gameObject.CompareTag("Player")&&!PlayerActivated) {
+
+            PlayerActivated=true;
+            StartCoroutine("LabStartUp");
+        
+        
+        }
+    
+    }
+
+    public IEnumerator LabStartUp() {
+
+
+        int goes = 0;
+        float timer = 0f;
+
+        while (goes < 4) {
+
+            timer += Time.deltaTime;
+
+            if (timer >= 0.2f) {
+
+                timer = 0f;
+                LabOn = !LabOn;
+                goes++;
+            
+            }
+        
+        
+            yield return null;
+        }
+
+
+        yield return new WaitForSeconds(1f);
+
+        LabOn = true;
+
+        yield return new WaitForSeconds(0.25f);
+
+
+        Destroy(gameObject);
+
+    }
+
+
+}
